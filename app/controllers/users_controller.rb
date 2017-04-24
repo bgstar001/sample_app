fa0b75fb987c643aeb4ab63b3e1bca8beed7aeaf
Thu Ngo <bgstar001@gmail.com>
2017-04-24 09:04:38 +0700

    chapter 8

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,15 +5,20 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
-  end 
-  
+    unless @user
+      flash[:danger] = t "users_not_found"
+      redirect_to root_path
+    end
+  end
+
   def create
-    @user = User.new(user_params)
+    @user = User.new user_params
     if @user.save
-      flash[:success] = "Welcome to the Sample App!"
+      log_in @user
+      flash[:success] = t "welcome"
       redirect_to @user
     else
-      render "new"
+      render :new
     end
   end
 
