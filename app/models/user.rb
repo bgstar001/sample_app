1bb3334effda07a1922565d8921ba1cdea4cd28d
Thu Ngo <bgstar001@gmail.com>
2017-05-19 18:10:07 +0700

    chapter 14

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,20 +4,39 @@ class User < ApplicationRecord
   attr_accessor :remember_token, :activation_token, :reset_token
 
   has_many :microposts, dependent: :destroy
+  has_many :active_relationships, class_name: Relationship.name,
+    foreign_key: "follower_id", dependent: :destroy
+  has_many :passive_relationships, class_name:  Relationship.name,
+    foreign_key: "followed_id", dependent: :destroy
+  has_many :following, through: :active_relationships, source: :followed
+  has_many :followers, through: :passive_relationships
+
   validates :name, presence: true, length: {maximum: 50}
   validates :email, presence: true, length: {maximum: 50},
     format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
   validates :password, length: {minimum: 6}, allow_nil: true
 
   has_secure_password
-  
+
   before_save :email_downcase
   before_create :create_activation_digest
 
   scope :activated, ->{where activated: true}
 
   def feed
-    microposts.ordered
+    Micropost.posts_feed(self).ordered
+  end
+
+  def follow other_user
+    following << other_user
+  end
+
+  def unfollow other_user
+    following.delete other_user
+  end
+
+  def following? other_user
+    following.include? other_user
   end
 
   def create_password_digest
@@ -60,7 +79,7 @@ class User < ApplicationRecord
   def send_activation_email
     UserMailer.account_activation(self).deliver_now
   end
-  
+
   class << self
     def new_token
       SecureRandom.urlsafe_base64
--- config/routes.rb
+++ config/routes.rb
@@ -12,8 +12,11 @@ Rails.application.routes.draw do
   get "/login", to: "sessions#new"
   post "/login", to: "sessions#create"
   delete "/logout", to: "sessions#destroy"
-  resources :users
+  resources :users, except: :new do
+    resources :relationships, only: :index
+  end
   resources :account_activations, only: :edit
   resources :password_resets, except: [:show, :index, :destroy]
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 end
